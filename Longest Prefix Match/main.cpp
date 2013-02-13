3d7e8f7b4a91d9076135c21a6db46d4bdf08046c
Filip Zapletal <punicz@gmail.com>
2013-02-13 12:20:02 +0100

    Project init

---
+++ Longest Prefix Match/main.cpp
@@ -0,0 +1,30 @@
+// Longest Prefix Match.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <csignal> 
+
+using namespace std;
+
+int main(int argc, char **argv) {   
+    cout << "test";
+    
+
+}
+
+
+//
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	return 0;
+//}
+
