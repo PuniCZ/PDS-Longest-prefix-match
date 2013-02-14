4eb646936628fa7e56f3bab1000d036c717a53f0
Filip Zapletal <punicz@gmail.com>
2013-02-14 14:47:05 +0100

    Converted to VS2010 project

--- Longest Prefix Match/ASMgr.h
+++ Longest Prefix Match/ASMgr.h
@@ -26,19 +26,6 @@ public:
         addr_v4 &= (0xffffffff << (32 - prefix));
     }
 
-    ASRecord(int address[4], int prefix, int asId)
-        :addr_v4(0)
-    {
-        int shift = 24;      
-        for (int i = 0; i < 4; i++, shift -= 8)
-        {
-            addr_v4 |= address[i] << shift ;
-        }
-
-        addr_v4 &= (0xffffffff << (32 - prefix));
-    }
-    
-
 private:
     int32_t addr_v4;
     int64_t addr_v6[2];
